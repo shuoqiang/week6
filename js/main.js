4f941b5f519949756d0750450bb60043611b94c0
Shuo Qiang <shuo.qiang@f1006-09-34181.bmcc.cuny.edu>
2015-10-26 14:24:36 -0400

    add top and bottom box

--- js/main.js
+++ js/main.js
@@ -6,17 +6,18 @@ function setup() {
     //http://www.colors.commutercreative.com/grid/
     var x = 0;
     var y = 0;
-    var gridWidth = width-100;
+    var gridWidth = width-500;
+    var x1,y1;
     for(var i=0; i<100; i++){
     //increment our x
-    x += 120;//same as x= x+120
+    x += 200;//same as x= x+200
     drawWindow(x,y);
     //check if x is past the grid width
     if(x >gridWidth){
         //set the x back to the beinning
         x =0;
         //move y down
-        y += 270;
+        y += 300;
     }//end if
     }//end loop
 }//end setup
@@ -31,10 +32,17 @@ function setup() {
       var winWidth = 100;
       var winHeight = 200;
   rect(startX,startY,winWidth,winHeight);
-  rect
   line(startX+winWidth/2,startY,startX+winWidth/2,startY+winHeight);
   line(startX,startY+winHeight/3,startX+winWidth,startY+winHeight/3);
   line(startX,startY+winHeight*.66,startX+winWidth,startY+winHeight*.66);
+  //add box at the bottom
+  x1 = startX - 20;
+  y1 = startY + winHeight;
+  rect(x1,y1,winWidth+40,20);
+  //add box at the top
+  x1 = startX-20;
+  y1 = startY-20;
+  rect(x1,y1,winWidth+40,20);
   }
 
 
